e314dd1fbff479c84706de249ca96f14f54525bf
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2018-04-11 13:03:52 -0400

    Fixed some minor formatting

--- prog.c
+++ prog.c
@@ -1,13 +1,13 @@
-    #include <stdio.h>
-    #include <dirent.h>
-    #include <string.h>
-    #include <sys/stat.h>
-    #include <sys/types.h>
-    #include <errno.h>
-    #include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <unistd.h>
 
-    int main(int argc, char** argv)
-    {
+int main(int argc, char **argv)
+{
     if (argc != 3)
     {
         printf("Provide an argument and directory path.\n");
@@ -18,31 +18,30 @@
     struct dirent *entryPtr;
     struct stat statBuf;
 
-    char* command = argv[1];
-    char* directory = argv[2];
+    char *command = argv[1];
+    char *directory = argv[2];
 
     chdir(directory);
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
-    printf("%s\n", cwd);
 
-    dirPtr = opendir (directory);
+    dirPtr = opendir(directory);
 
-    while ((entryPtr = readdir (dirPtr)))
+    while ((entryPtr = readdir(dirPtr)))
     {
-        printf ("%-20s", entryPtr->d_name);
+        printf("%-20s", entryPtr->d_name);
         stat(entryPtr->d_name, &statBuf);
 
-        if(strcmp(command, "-i") == 0)
+        if (strcmp(command, "-i") == 0)
         {
             printf("\tInode: %lld\n", statBuf.st_ino);
         }
-        else if(strcmp(command, "-n") == 0)
+        else if (strcmp(command, "-n") == 0)
         {
             printf("\tUser: %d\t Group: %d\n", statBuf.st_uid, statBuf.st_gid);
         }
     }
 
-    closedir (dirPtr);
+    closedir(dirPtr);
     return 0;
-    }
+}
